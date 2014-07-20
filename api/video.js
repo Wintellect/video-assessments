b67b6a380a35c094227d0414882227bcbc10c539
Dave Baskin <dfbaskin@stanza.com>
2014-07-19 20:10:24 -0400

    Adding streaming video API.

--- api/video.js
+++ api/video.js
@@ -9,6 +9,63 @@ module.exports.initializer = function(app) {
 
     app.use(busboy());
 
+    app.get("/api/video/:id", function(req, res) {
+        var projectId = req.param("id");
+        var fileName, fileStat, fileRange, match;
+        var totalRange, rangeStart, rangeEnd, rangeSize;
+        if(!projectId.match(projectIdRegex)) {
+            sendError(res, 400, 'Invalid project id.');
+        }
+        else {
+            fileName = buildProjectFileName(projectId, ".mp4");
+            if (!fs.existsSync(fileName)) {
+                sendError(res, 400, 'Project not found.');
+            }
+            else {
+
+                fileStat = fs.statSync(fileName);
+                totalRange = fileStat.size;
+
+                console.log([
+                    "Requested Range:", req.headers.range
+                ].join(" "));
+                match = (req.headers.range || "").match(/^bytes=(\d+)-(\d+)?$/i);
+                if(match) {
+                    rangeStart = parseInt(match[1], 10);
+                    rangeEnd = match[2] ? parseInt(match[2], 10) : totalRange - 1;
+                    rangeSize = rangeEnd - rangeStart + 1;
+                    console.log([
+                        'Video', projectId,
+                        'Size:', totalRange,
+                        'Range:', rangeStart, "-", rangeEnd
+                    ].join(" "));
+                    res.writeHead(206, {
+                        'Content-Range': 'bytes ' + rangeStart + '-' + rangeEnd + '/' + totalRange,
+                        'Accept-Ranges': 'bytes',
+                        'Content-Length': rangeSize,
+                        'Content-Type': 'video/mp4'
+                    });
+                    fs.createReadStream(fileName, {
+                        start: rangeStart,
+                        end: rangeEnd
+                    }).pipe(res);
+                }
+                else {
+                    console.log([
+                        'Video', projectId,
+                        'Size:', totalRange,
+                        'Range: ALL'
+                    ].join(" "));
+                    res.writeHead(200, {
+                        'Content-Length': fileStat.size,
+                        'Content-Type': 'video/mp4'
+                    });
+                    fs.createReadStream(fileName).pipe(res);
+                }
+            }
+        }
+    });
+
     app.put("/api/video/:id", function(req, res) {
         var projectId = req.param("id");
         var fileName, fileStream;
